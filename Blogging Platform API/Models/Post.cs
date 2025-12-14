2f6cf953d3c847211bcc6b335786e104f09aa7cb
Pablo <ortegaperaltap@gmail.com>
2025-12-14 00:01:09 -0400

    create Post model

---
+++ Blogging Platform API/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace Blogging_Platform_API.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get;set; }
+        public string? Category { get; set; }
+        public List<string>? Tags { get; set; }
+        public required User Author { get; set; }
+    }
+}
