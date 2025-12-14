1277482759f2fdabf6f2ca62b8d87ba1ceabe3cf
Pablo <ortegaperaltap@gmail.com>
2025-12-13 23:38:15 -0400

    create controller for blog posts

---
+++ Blogging Platform API/Controllers/BlogPostsControllers.cs
@@ -0,0 +1,17 @@
+﻿using Blogging_Platform_API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogging_Platform_API.Controllers
+{
+    [ApiController]
+    [Route("/posts")]
+    public class BlogPostsControllers : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public BlogPostsControllers(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
