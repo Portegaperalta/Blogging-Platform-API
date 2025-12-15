ad4831ac599b77d6e06a844dd361a38b7335f54f
Pablo <ortegaperaltap@gmail.com>
2025-12-14 22:45:46 -0400

    Add [required ]data annotations to name, email and password

--- Blogging Platform API/Models/User.cs
+++ Blogging Platform API/Models/User.cs
@@ -1,10 +1,15 @@
-﻿namespace Blogging_Platform_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blogging_Platform_API.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public required string Name { get; set; }
+        [Required]
         public required string Email { get; set; }
+        [Required]
         public required string Password { get; set; }
         public List<Post>? Posts { get; set; }
     }
