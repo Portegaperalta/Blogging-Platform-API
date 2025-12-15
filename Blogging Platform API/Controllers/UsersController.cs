5c456f5a2dfb798d3a122a7ee0aeca67457c5c70
Pablo <ortegaperaltap@gmail.com>
2025-12-14 22:32:09 -0400

    Create UsersController and add GET request to return all users

---
+++ Blogging Platform API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Blogging_Platform_API.Data;
+using Blogging_Platform_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogging_Platform_API.Controllers
+{
+    [ApiController]
+    [Route("/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        //GET: /users
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await context.Users.ToListAsync();
+        }
+    }
+}
