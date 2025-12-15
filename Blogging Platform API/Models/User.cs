211cb3c3d711c39b6485ab56314164710bc52d1a
Pablo <ortegaperaltap@gmail.com>
2025-12-14 23:13:26 -0400

    add custom error messages to [required] data annotations

--- Blogging Platform API/Models/User.cs
+++ Blogging Platform API/Models/User.cs
@@ -5,11 +5,11 @@ namespace Blogging_Platform_API.Models
     public class User
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="The field {0} is required")]
         public required string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage ="The field {0} is required")]
         public required string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage ="The field {0} is required")]
         public required string Password { get; set; }
         public List<Post>? Posts { get; set; }
     }
