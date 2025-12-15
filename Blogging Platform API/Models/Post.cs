b96bbc5a477fc8d549297d870b37731353fd1c8b
Pablo <ortegaperaltap@gmail.com>
2025-12-14 22:22:11 -0400

    Remove EF Core migrations and update Post.Tags property
    Deleted migration and snapshot files to reset EF Core history and DB

--- Blogging Platform API/Models/Post.cs
+++ Blogging Platform API/Models/Post.cs
@@ -6,7 +6,7 @@
         public required string Title { get; set; }
         public required string Content { get;set; }
         public string? Category { get; set; }
-        public List<string>? Tags { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
         public required User Author { get; set; }
     }
 }
