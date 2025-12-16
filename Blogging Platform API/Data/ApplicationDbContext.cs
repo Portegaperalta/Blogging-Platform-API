bdbb190f202f683c9218e55167b8cce01ac09f71
Pablo <ortegaperaltap@gmail.com>
2025-12-16 04:06:40 -0400

    add Dbset<Post>

--- Blogging Platform API/Data/ApplicationDbContext.cs
+++ Blogging Platform API/Data/ApplicationDbContext.cs
@@ -5,10 +5,11 @@ namespace Blogging_Platform_API.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base (options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
     }
 }
