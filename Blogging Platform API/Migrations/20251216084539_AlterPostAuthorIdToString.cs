fc95ad22c4e2719f64f5263c8d96dd17894a2b13
Pablo <ortegaperaltap@gmail.com>
2025-12-16 05:57:36 -0400

    change Author Id type from string to int and create migration to save changes to db

---
+++ Blogging Platform API/Migrations/20251216084539_AlterPostAuthorIdToString.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogging_Platform_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterPostAuthorIdToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_AuthorId1",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_AuthorId1",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId1",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_AuthorId",
+                table: "Posts",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId1",
+                table: "Posts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId1",
+                table: "Posts",
+                column: "AuthorId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_AuthorId1",
+                table: "Posts",
+                column: "AuthorId1",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
--- Blogging Platform API/Models/Post.cs
+++ Blogging Platform API/Models/Post.cs
@@ -10,7 +10,7 @@ namespace Blogging_Platform_API.Models
         public required string Content { get;set; }
         public string? Category { get; set; }
         public List<string> Tags { get; set; } = new List<string>();
-        public required string AuthorId { get; set; }
+        public required int AuthorId { get; set; }
         public User? Author { get; set; }
     }
 }
