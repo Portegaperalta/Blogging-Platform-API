b950d197a5221a3d5de1132fc6cd8d9aa7620b02
Pablo <ortegaperaltap@gmail.com>
2025-12-16 04:20:23 -0400

    add request to get post by id

--- Blogging Platform API/Controllers/BlogPostsControllers.cs
+++ Blogging Platform API/Controllers/BlogPostsControllers.cs
@@ -16,11 +16,26 @@ namespace Blogging_Platform_API.Controllers
             this.context = context;
         }
 
-        //GET : /posts
+        //GET: /posts
         [HttpGet]
         public async Task<IEnumerable<Post>> Get()
         {
             return await context.Posts.ToListAsync();
+        }
+
+        //GET: /posts/id
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Post>> Get([FromRoute]int id)
+        {
+            var post = await context.Posts.
+                       FirstOrDefaultAsync(x => x.Id == id);
+
+            if (post is null)
+            {
+                return NotFound();
+            }
+
+            return post;
         }
     }
 }
