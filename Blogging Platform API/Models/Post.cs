17ce93faf81c0b179d146d32db54ef0b1ddb1e57
Pablo <ortegaperaltap@gmail.com>
2025-12-16 04:39:50 -0400

    add AuthorId attribute and declare Author attribute as nullable

--- Blogging Platform API/Models/Post.cs
+++ Blogging Platform API/Models/Post.cs
@@ -10,6 +10,7 @@ namespace Blogging_Platform_API.Models
         public required string Content { get;set; }
         public string? Category { get; set; }
         public List<string> Tags { get; set; } = new List<string>();
-        public required User Author { get; set; }
+        public required string AuthorId { get; set; }
+        public User? Author { get; set; }
     }
 }
