adc081e773e37cf5ee1fa63d59acbe313f8915ad
Pablo <ortegaperaltap@gmail.com>
2025-12-18 09:03:21 -0400

    create interface for BlogPostService

---
+++ Blogging Platform API/Services/IBlogPostService.cs
@@ -0,0 +1,13 @@
+﻿using Blogging_Platform_API.Models;
+
+namespace Blogging_Platform_API.Services
+{
+    public interface IBlogPostService
+    {
+        Task<IEnumerable<Post>> GetBlogPostsAsync();
+        Task<Post?> GetBlogPostByIdAsync(int id);
+        Task CreateBlogPostAsync(Post post);
+        Task UpdateBlogPostAsync(int id,Post post);
+        Task<int> DeleteBlogPostAsync(int id);
+    }
+}
