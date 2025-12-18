9e1dd72bdab29f51111f8431fc73c683509b59bb
Pablo <ortegaperaltap@gmail.com>
2025-12-17 21:31:44 -0400

    Create interface UserService with all its signatures

---
+++ Blogging Platform API/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Blogging_Platform_API.Models;
+
+namespace Blogging_Platform_API.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
