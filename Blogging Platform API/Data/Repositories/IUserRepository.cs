377754531c96a729bb758cd1509711acf983aeb0
Pablo <ortegaperaltap@gmail.com>
2025-12-17 20:31:56 -0400

    Create Repositories folder and create IUserRepository file

---
+++ Blogging Platform API/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blogging_Platform_API.Models;
+
+namespace Blogging_Platform_API.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetUsers();
+        User? GetUserById(int id);
+        void CreateUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+    }
+}
