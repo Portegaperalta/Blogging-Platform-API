4b9972c1f3bfbfa84e84241d628b313f8ff653f2
Pablo <ortegaperaltap@gmail.com>
2025-12-17 22:39:54 -0400

    add UserRepositoy and UserService in services area

--- Blogging Platform API/Program.cs
+++ Blogging Platform API/Program.cs
@@ -1,5 +1,7 @@
 using System.Text.Json.Serialization;
 using Blogging_Platform_API.Data;
+using Blogging_Platform_API.Data.Repositories;
+using Blogging_Platform_API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,8 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 options.JsonSerializerOptions.ReferenceHandler =
 ReferenceHandler.IgnoreCycles);
 
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
