94d24d7ea5d30ab73535dd2ad87d0dead4566072
Pablo <ortegaperaltap@gmail.com>
2025-12-18 08:39:28 -0400

    create interface for BlogPostRepository

---
+++ Blogging Platform API/Data/Repositories/IBlogPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blogging_Platform_API.Models;
+
+namespace Blogging_Platform_API.Data.Repositories
+{
+    public interface IBlogPostRepository
+    {
+        Task<IEnumerable<Post>> GetBlogPostsAsync();
+        Task<Post?> GetBlogPostByIdAsync(int id);
+        Task CreateBlogPostAsync();
+        Task UpdateBlogPostAsync(Post post);
+        Task DeleteBlogPostAsync(int id);
+    }
+}
